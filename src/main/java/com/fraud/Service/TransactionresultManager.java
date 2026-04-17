5c87db73673be87b7e04348b8753dce7b6661c3b
Govind Chaurasiya <cgovind350@gmail.com>
2026-04-17 23:00:15 +0530

    Feature: Integrate Redis to scale up on multiple instance

--- src/main/java/com/fraud/Service/TransactionresultManager.java
+++ src/main/java/com/fraud/Service/TransactionresultManager.java
@@ -3,10 +3,12 @@ package com.fraud.Service;
 import com.fraud.model.TransactionScored;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Component;
 import tools.jackson.databind.ObjectMapper;
 
+import java.time.Duration;
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ConcurrentHashMap;
@@ -20,6 +22,7 @@ public class TransactionresultManager {
     private final ObjectMapper objectMapper;
     private final Map<String, CompletableFuture<TransactionScored>> pendingRequests = new ConcurrentHashMap<>();
     private final Map<String, TransactionScored> completedResults = new ConcurrentHashMap<>();
+    private static final String REDIS_KEY_PREFIX = "txn:score";
 
     public CompletableFuture<TransactionScored> createPendingRequest(String transactionId)
     {
@@ -30,17 +33,26 @@ public class TransactionresultManager {
 
     public void completeRequest(TransactionScored result)
     {
-        completedResults.put(result.getTransactionId(), result);
-        CompletableFuture<TransactionScored> future = pendingRequests.remove(result.getTransactionId());
+        String jsonResult = objectMapper.writeValueAsString(result);
+        redisTemplate.opsForValue().set(REDIS_KEY_PREFIX + result.getTransactionId(), jsonResult, Duration.ofHours(1));
+
+        CompletableFuture<TransactionScored>future = pendingRequests.remove(result.getTransactionId());
         if(future != null)
         {
             future.complete(result);
         }
+
     }
 
     public TransactionScored getCompletedResult(String transactionID)
     {
-        return completedResults.get(transactionID);
+        String jsonResult = redisTemplate.opsForValue().get(REDIS_KEY_PREFIX + transactionID);
+
+        if(jsonResult !=null)
+        {
+            return objectMapper.readValue(jsonResult, TransactionScored.class);
+        }
+        return null;
     }
 
 }
